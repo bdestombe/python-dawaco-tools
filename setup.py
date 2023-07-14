13eac65abd21019e32ba36e06d6457eb6bdb12eb
Bas des Tombe <bdestombe@gmail.com>
2023-07-14 09:20:18 +0200

    updated install_requires

--- setup.py
+++ setup.py
@@ -3,24 +3,37 @@ from os import path
 from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     l_d = f.read()
 
 setup(
-    name='dawacotools',
-    version='0.0.1',
-    description='Dawaco tools by PWN',
+    name="dawacotools",
+    version="0.0.1",
+    description="Dawacotools by PWN",
     long_description=l_d,
     long_description_content_type="text/markdown",
-    url='https://github.com/bdestombe/python-dawaco-tools',
-    author='Bas des Tombe',
-    license='MIT',
+    url="https://github.com/bdestombe/python-dawaco-tools",
+    author="Bas des Tombe",
+    license="MIT",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
-    install_requires=['sqlalchemy', 'geopandas', 'matplotlib', 'pyodbc', 'xarray', 'netcdf4'],
+    install_requires=[
+        "sqlalchemy",
+        "pandas[parquet,feather,performance,excel]",
+        "geopandas",
+        "matplotlib",
+        "pyodbc",
+        "xarray",
+        "netcdf4",
+        "pytest",
+        "openpyxl",
+        "rasterio",
+        "contextily",
+        "hydropandas"
+    ],
 )
