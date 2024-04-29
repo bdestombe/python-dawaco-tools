622ba87a316db38fb5998bb736e80f27e1f0793a
Bas des Tombe <bdestombe@gmail.com>
2024-04-29 13:25:39 +0200

    output folder structure for workflows

--- workflows/TRA debiet/mps_debiet.py
+++ workflows/TRA debiet/mps_debiet.py
@@ -1,3 +1,6 @@
+import os
+
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -25,12 +28,14 @@ fils["sec_nput"] = fils["sec_flow_tag"].replace(counts)
 sec_flows = dw.io_plenty.get_sec_pa_flows(df_plenty)
 
 # save unaltered version to feather
-sec_flows.reset_index().to_feather("sec_flows.feather")
+fp = os.path.join(__file__, "output", "sec_flows.feather")
+sec_flows.reset_index().to_feather(fp)
 
 # alter data
 sec_flows_altered = sec_flows.iloc[-10:].copy()
 sec_flows_altered = sec_flows_altered + (np.random.rand(*sec_flows_altered.shape) - 0.5) * sec_flows_altered
-sec_flows_altered.reset_index().to_feather("sec_flows_altered.feather")
+fp = os.path.join(__file__, "output", "sec_flows_altered.feather")
+sec_flows_altered.reset_index().to_feather(fp)
 
 sec_flows = sec_flows.groupby(sec_flows.index.year).mean()
 sec_flows.index = "flow_" + sec_flows.index.astype(str)
@@ -39,12 +44,10 @@ fils[sec_flows.index] = np.nan
 
 for tag, nput in counts.items():
     fils.loc[fils["sec_flow_tag"] == tag, sec_flows.index] = sec_flows[tag].values / nput
-
-fils.to_file("pumping_infiltration_wells.geojson", driver="GeoJSON")
+fp = os.path.join(__file__, "output", "pumping_infiltration_wells.geojson")
+fils.to_file(fp, driver="GeoJSON")
 
 # altered data
-import geopandas as gpd
-
 x = fils.x + (np.random.rand(len(fils)) - 0.5) * 250
 y = fils.y + (np.random.rand(len(fils)) - 0.5) * 250
 del fils["x"]
@@ -52,4 +55,5 @@ del fils["y"]
 fils = gpd.GeoDataFrame(fils, geometry=gpd.points_from_xy(x, y), crs="EPSG:28992")
 
 fils[sec_flows.index] = fils[sec_flows.index] + (np.random.rand(len(fils), len(sec_flows.index)) - 0.5) * fils[sec_flows.index]
-fils.to_file("pumping_infiltration_wells_altered.geojson", driver="GeoJSON")
+fp = os.path.join(__file__, "output", "pumping_infiltration_wells_altered.geojson")
+fils.to_file(fp, driver="GeoJSON")
