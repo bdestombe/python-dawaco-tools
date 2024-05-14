bbba13a37014b1af41307ca97b26845dcb457075
Bas des Tombe <bdestombe@gmail.com>
2024-05-14 14:27:08 +0200

    MFA required. Implementation does not cache token, thus MFA is required every time

--- dawacotools/io.py
+++ dawacotools/io.py
@@ -10,11 +10,11 @@ from sqlalchemy.engine import URL
 
 # Test server
 connection_string = (
-    "DRIVER={ODBC Driver 17 for SQL Server};"
-    "SERVER=pwnka-p-we-prd-dawaco-sql.database.windows.net;"
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=tcp:pwnka-p-we-prd-dawaco-sql.database.windows.net;"
     "PORT=1433;"
     "DATABASE=Dawacoprod;"
-    "Trusted_Connection=yes;"
+    "Authentication=ActiveDirectoryInteractive;"
 )
 dbname = "dbo"
 
